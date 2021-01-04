0ec18986337c79394717edc81031009b267d6859
Peter Hagane <peterhagane@yahoo.no>
2021-01-04 23:03:58 +0100

    Fix anchor links

--- js/navbar.js
+++ js/navbar.js
@@ -12,6 +12,7 @@ var html = `
 
 <div class="navbarright">
 <ul class="menu">
+    <li><a href="#home">Home</a></li>
     <li><a href="#about">About</a></li>
     <li><a href="#projects">Projects</a></li>
     <li><a href="#contact">Contact</a></li>
